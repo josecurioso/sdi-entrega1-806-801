f5978622c6d52d6124646c03c8b3932b4e137147
Luis <UO257809@uniovi.es>
2019-03-14 14:34:55 +0100

    Pruebas 28-29 añadidas

--- src/test/java/com/uniovi/tests/SDIWallapopApplicationTests.java
+++ src/test/java/com/uniovi/tests/SDIWallapopApplicationTests.java
@@ -3,9 +3,7 @@ package com.uniovi.tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,6 +11,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -20,36 +19,31 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import com.uniovi.entities.Offer;
-import com.uniovi.entities.User;
-import com.uniovi.services.InsertSampleDataService;
-import com.uniovi.services.RolesService;
-import com.uniovi.services.UsersService;
 import com.uniovi.tests.pageobject.PO_LoginView;
 import com.uniovi.tests.pageobject.PO_NavView;
 import com.uniovi.tests.pageobject.PO_Properties;
 import com.uniovi.tests.pageobject.PO_RegisterView;
 import com.uniovi.tests.pageobject.PO_View;
 import com.uniovi.utils.SeleniumUtils;
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SDIWallapopApplicationTests {
 	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
 	// automáticas)):
-	
 
 	// Luis
 	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
 	static String Geckdriver024 = "C:\\Users\\Pedro\\Desktop\\SDI\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
-	//automáticas)):
+	// automáticas)):
 
 	// Jose
 //	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
 //	static String Geckdriver024 = "D:\\Escritorio\\geckodriver024win64.exe";
-
-
 
 	// Común a Windows y a MACOSX
 	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
@@ -62,11 +56,10 @@ public class SDIWallapopApplicationTests {
 		return driver;
 	}
 
-	
 	// Antes de cada prueba se navega al URL home de la aplicaciónn
 	@Before
 	public void setUp() {
-	
+
 		driver.navigate().to(URL);
 	}
 
@@ -87,172 +80,189 @@ public class SDIWallapopApplicationTests {
 		// Cerramos el navegador al finalizar las pruebas
 		driver.quit();
 	}
-	
-	
+
 	@Test
 	public void PR01() {
 		PO_NavView.clickOption(driver, "signup", "class", "btn btn-primary");
-		PO_RegisterView.fillForm(driver, "UO257809@uniovi.es", "Luis", "pastrana", "77777",
-		"77777");
+		PO_RegisterView.fillForm(driver, "UO257809@uniovi.es", "Luis", "pastrana", "77777", "77777");
 		PO_View.checkElement(driver, "text", "Bienvenido a WallapopSDI");
 	}
-	
+
 	@Test
 	public void PR02() {
 		PO_NavView.clickOption(driver, "signup", "class", "btn btn-primary");
-		PO_RegisterView.fillForm(driver, "", "", "", "77777",
-		"77777");
+		PO_RegisterView.fillForm(driver, "", "", "", "77777", "77777");
 		PO_View.checkElement(driver, "text", "Registrate");
 	}
-	
-	
+
 	@Test
 	public void PR03() {
 		PO_NavView.clickOption(driver, "signup", "class", "btn btn-primary");
-		PO_RegisterView.fillForm(driver, "UO257809@uniovi.es", "Luis", "pastrana", "77777",
-		"777773434");
+		PO_RegisterView.fillForm(driver, "UO257809@uniovi.es", "Luis", "pastrana", "77777", "777773434");
 		PO_View.checkElement(driver, "text", "Registrate");
 	}
-	
+
 	@Test
 	public void PR04() {
 		PO_NavView.clickOption(driver, "signup", "class", "btn btn-primary");
-		PO_RegisterView.fillForm(driver, "marta@gmail.com", "marta", "pastrana", "77777",
-		"77777");
+		PO_RegisterView.fillForm(driver, "marta@gmail.com", "marta", "pastrana", "77777", "77777");
 		PO_View.checkElement(driver, "text", "Registrate");
 	}
-	
-	
+
 	@Test
 	public void PR05() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, "admin@gmail.com" , "admin" );
+		PO_LoginView.fillForm(driver, "admin@gmail.com", "admin");
 		PO_NavView.checkElement(driver, "id", "users-menu");
 	}
+
 	@Test
 	public void PR06() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, "marta@gmail.com" , "123456" );
+		PO_LoginView.fillForm(driver, "marta@gmail.com", "123456");
 		PO_View.checkElement(driver, "id", "offers-menu");
 	}
+
 	@Test
 	public void PR07() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, "a" , "a" );
-		PO_LoginView.checkKey(driver, "Error.login.incorrect",
-		PO_Properties.getSPANISH() );
+		PO_LoginView.fillForm(driver, "a", "a");
+		PO_LoginView.checkKey(driver, "Error.login.incorrect", PO_Properties.getSPANISH());
 	}
+
 	@Test
 	public void PR08() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, "marta@gmail.com" , "noesesta" );
-		PO_LoginView.checkKey(driver, "Error.login.incorrect",
-		PO_Properties.getSPANISH() );
+		PO_LoginView.fillForm(driver, "marta@gmail.com", "noesesta");
+		PO_LoginView.checkKey(driver, "Error.login.incorrect", PO_Properties.getSPANISH());
 	}
+
 	@Test
 	public void PR09() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, "no@existe.com" , "noexiste" );
-		PO_LoginView.checkKey(driver, "Error.login.incorrect",
-		PO_Properties.getSPANISH() );
+		PO_LoginView.fillForm(driver, "no@existe.com", "noexiste");
+		PO_LoginView.checkKey(driver, "Error.login.incorrect", PO_Properties.getSPANISH());
 	}
-	
-	//logout
+
+	// logout
 	@Test
 	public void PR10() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, "marta@gmail.com" , "123456" );
+		PO_LoginView.fillForm(driver, "marta@gmail.com", "123456");
 		PO_View.checkElement(driver, "id", "offers-menu");
 		PO_NavView.clickOption(driver, "logout", "class", "btn btn-primary");
 		PO_View.checkElement(driver, "text", "Identificate");
 	}
+
 	@Test
 	public void PR11() {
 		(new WebDriverWait(driver, 200)).until(ExpectedConditions.invisibilityOfElementLocated(By.id("logout")));
 
 	}
+
 	@Test
 	public void PR12() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, "admin@gmail.com" , "admin" );
+		PO_LoginView.fillForm(driver, "admin@gmail.com", "admin");
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id,'users-menu')]/a");
 		elementos.get(0).click();
 		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, 'user/list')]");
 		elementos.get(0).click();
-	
-		List<WebElement> elementos2 = SeleniumUtils.EsperaCargaPagina(driver, "free",
-		"//tbody/tr", PO_View.getTimeout());
-		assertEquals(elementos2.size() , 7);
+
+		List<WebElement> elementos2 = SeleniumUtils.EsperaCargaPagina(driver, "free", "//tbody/tr",
+				PO_View.getTimeout());
+		assertEquals(elementos2.size(), 7);
 	}
+
 	@Test
 	public void PR13() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, "admin@gmail.com" , "admin" );
+		PO_LoginView.fillForm(driver, "admin@gmail.com", "admin");
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id,'users-menu')]/a");
 		elementos.get(0).click();
 		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, 'user/list')]");
 		elementos.get(0).click();
-	
-		List<WebElement> usersList = SeleniumUtils.EsperaCargaPagina(driver, "free",
-		"//tbody/tr", PO_View.getTimeout());
-		assertTrue(usersList.size() == 6);//igual el num esta mal
-		
-		
-		
-		usersList.get(0).findElement(By.id("idsUsers")).click();	
-        
-        List<WebElement> listPage = PO_View.checkElement(driver, "free", "//*[contains(@id,'btnDelete')]");
-        listPage.get(0).click();
-        assertTrue(usersList.size() == 5);//esto seria 1 menos
+
+		List<WebElement> usersList = SeleniumUtils.EsperaCargaPagina(driver, "free", "//tbody/tr",
+				PO_View.getTimeout());
+		assertTrue(usersList.size() == 6);// igual el num esta mal
+
+		usersList.get(0).findElement(By.id("idsUsers")).click();
+
+		List<WebElement> listPage = PO_View.checkElement(driver, "free", "//*[contains(@id,'btnDelete')]");
+		listPage.get(0).click();
+		assertTrue(usersList.size() == 5);// esto seria 1 menos
 
 	}
+
 	@Test
 	public void PR14() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, "admin@gmail.com" , "admin" );
+		PO_LoginView.fillForm(driver, "admin@gmail.com", "admin");
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id,'users-menu')]/a");
 		elementos.get(0).click();
 		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, 'user/list')]");
 		elementos.get(0).click();
-	
-		List<WebElement> usersList = SeleniumUtils.EsperaCargaPagina(driver, "free",
-		"//tbody/tr", PO_View.getTimeout());
-		assertTrue(usersList.size() == 6);//igual el num esta mal
-		
-		
-		
-		usersList.get(usersList.size()).findElement(By.id("idsUsers")).click();	
-        
-        List<WebElement> listPage = PO_View.checkElement(driver, "free", "//*[contains(@id,'btnDelete')]");
-        listPage.get(0).click();
-        assertTrue(usersList.size() == 5);//esto seria 1 menos
-        
+
+		List<WebElement> usersList = SeleniumUtils.EsperaCargaPagina(driver, "free", "//tbody/tr",
+				PO_View.getTimeout());
+		assertTrue(usersList.size() == 6);// igual el num esta mal
+
+		usersList.get(usersList.size()).findElement(By.id("idsUsers")).click();
+
+		List<WebElement> listPage = PO_View.checkElement(driver, "free", "//*[contains(@id,'btnDelete')]");
+		listPage.get(0).click();
+		assertTrue(usersList.size() == 5);// esto seria 1 menos
+
 	}
+
 	@Test
 	public void PR15() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
-		PO_LoginView.fillForm(driver, "admin@gmail.com" , "admin" );
+		PO_LoginView.fillForm(driver, "admin@gmail.com", "admin");
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id,'users-menu')]/a");
 		elementos.get(0).click();
 		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, 'user/list')]");
 		elementos.get(0).click();
-	
-		List<WebElement> usersList = SeleniumUtils.EsperaCargaPagina(driver, "free",
-		"//tbody/tr", PO_View.getTimeout());
-		assertTrue(usersList.size() == 6);//igual el num esta mal
-		
-		
-		
-		usersList.get(usersList.size()).findElement(By.id("idsUsers")).click();	
-		usersList.get(usersList.size()-1).findElement(By.id("idsUsers")).click();	
-		usersList.get(usersList.size()-2).findElement(By.id("idsUsers")).click();	
-        
-        List<WebElement> listPage = PO_View.checkElement(driver, "free", "//*[contains(@id,'btnDelete')]");
-        listPage.get(0).click();
-        assertTrue(usersList.size() == 5);//esto seria 1 menos
-        
+
+		List<WebElement> usersList = SeleniumUtils.EsperaCargaPagina(driver, "free", "//tbody/tr",
+				PO_View.getTimeout());
+		assertTrue(usersList.size() == 6);// igual el num esta mal
+
+		usersList.get(usersList.size()).findElement(By.id("idsUsers")).click();
+		usersList.get(usersList.size() - 1).findElement(By.id("idsUsers")).click();
+		usersList.get(usersList.size() - 2).findElement(By.id("idsUsers")).click();
+
+		List<WebElement> listPage = PO_View.checkElement(driver, "free", "//*[contains(@id,'btnDelete')]");
+		listPage.get(0).click();
+		assertTrue(usersList.size() == 5);// esto seria 1 menos
+
 	}
-	
+
+	@Test
+	public void PR28() {
+
+		driver.navigate().to(URL + "/user/list");
+		PO_LoginView.checkKey(driver, "login.message", PO_Properties.getSPANISH());
+
+	}
+
+	@Test
+	public void PR29() {
+
+		driver.navigate().to(URL + "/user/offer/list");
+		PO_LoginView.checkKey(driver, "login.message", PO_Properties.getSPANISH());
+
+	}
+
+	@Test
+	public void PR30() {
+
+		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_LoginView.fillForm(driver, "marta@gmail.com", "123456");
+		driver.navigate().to(URL + "/user/list");
+	}
+
 //	
 //	//PR06. Prueba del formulario de registro. DNI repetido en la BD, Nombre corto, .... pagination  pagination-centered, Error.signup.dni.length
 //	@Test
@@ -427,7 +437,5 @@ public class SDIWallapopApplicationTests {
 //	//Ahora nos desconectamos
 //	PO_PrivateView.clickOption(driver, "logout", "text", "Identifícate");
 //}
-	
-	
-	
+
 }
